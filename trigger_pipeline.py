817544edd6afc45cc346f5471a4476304a6d6f78
Raghavendra <raghus606@gmail.com>
2017-08-21 16:34:06 -0400

    Added code for triggering pipeline
    Former-commit-id: abd4698bb94a32ea379ae3302e83a0567ada44f0

---
+++ trigger_pipeline.py
@@ -0,0 +1,28 @@
+####### Trigger a luigi pipeline #######
+
+import ConfigParser
+import sys
+import subprocess
+
+def main(samples_config):
+    ''' The main function
+    '''
+
+    parser = ConfigParser.ConfigParser()
+    parser.read(samples_config)
+
+    for section in parser.sections():
+        sample_name = section
+        R1_fastq = parser.get(section,'R1_fastq')
+        R2_fastq = parser.get(section,'R2_fastq')
+
+        cmd = """PYTHONPATH="" LUIGI_CONFIG_PATH='./pipeline_wts.cfg' luigi --module single_cell_rnaseq JoinCountFiles --R1-fastq {R1} --R2-fastq {R2} --sample-name {sample} --workers 14 --worker-wait-interval 20""".format(R1=R1_fastq,R2=R2_fastq,sample=sample_name)
+        print "Running command : {cmd}\n".format(cmd=cmd)
+        p = subprocess.Popen(cmd,shell=True)
+        p.wait()
+        if p.returncode: ## Non zero returncode
+            print "Command failed : {cmd}\n".format(cmd=cmd)
+
+if __name__ == '__main__':
+    luigi_config = sys.argv[1]
+    main(luigi_config)
