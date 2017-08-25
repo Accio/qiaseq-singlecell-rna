9229cbf32e904af97d6403cc707d498baa30f56a
rpadmanabhan <raghus606@gmail.com>
2017-08-25 21:31:27 +0000

    Unified WTS and Targetted functions
    Former-commit-id: d667f3126331ffd70b46181e5b1cc3b492e94cca

--- core/merge_mt_files.py
+++ core/merge_mt_files.py
@@ -3,13 +3,23 @@ import os
 import sys
 from collections import defaultdict,OrderedDict
 
-def merge_count_files_wts(basedir,out_file,sample_name,ncells=384):
+        
+def float_to_string(val):
+    ''' Helper function to convert a float into a printable string
+
+    :param float val: the value to typecast
+    '''
+    return ('%.2f' % val).rstrip('0').rstrip('.')
+
+def merge_count_files(basedir,out_file,sample_name,ncells=384):
     ''' Merge count files from different cells
+
     :param str basedir: the basedirectory
     :param str out_file: the path to the output file
+    :param str sample_name: the name of the sample
+    :param bool wts: Whether it is whole transcriptome or not
     :param int ncells: the number of cell indices
     '''
-
     i = 0
     MT = defaultdict(lambda:defaultdict(int))
     cells = range(1,ncells+1)
@@ -21,13 +31,17 @@ def merge_count_files_wts(basedir,out_file,sample_name,ncells=384):
         cell = os.path.dirname(f).split('/')[-1].split('_')[0].strip('Cell')
         with open(f,'r') as IN:
             for line in IN:
-                chrom,start,stop,strand,gene,gene_type,mt = line.rstrip('\n').split('\t')
-                key = (chrom,start,stop,strand,gene,gene_type)
+                k1,k2,k3,k4,k5,k6,mt = line.rstrip('\n').split('\t')
+                key = (k1,k2,k3,k4,k5,k6)
                 MT[key][cell] = mt
     last_key = cell
 
+    if wts:
+        header = "chromosome\tstart\tstop\tstrand\tgene\tgene_type\t{cells}\n"
+    else:
+        header = "chromosome\tstart\tstop\tstrand\tgene\tprimer_sequence\t{cells}\n"
     with open(out_file,'w') as OUT:
-        print >> OUT,"chromosome\tstart\tstop\tstrand\tgene\tgene_type\t{cells}".format(cells=cell_header)
+        OUT.write(header.format(cells = cell_header))
         for key in MT:
             out = '\t'.join(key)
             for cell in cells:
@@ -35,15 +49,17 @@ def merge_count_files_wts(basedir,out_file,sample_name,ncells=384):
                     out = out + '\t0'
                 else:
                     out = out + '\t'+MT[key][str(cell)]
-            print >> OUT,out
+            OUT.write(out+'\n')
 
-def merge_metric_files_wts(basedir,temp_metric_file,metric_file,metric_file_cell,sample_name,ncells=384):
-    ''' Merge the metrics from gene finding
+def merge_metric_files(basedir,temp_metric_file,metric_file,metric_file_cell,sample_name,wts=True,ncells=384):
+    ''' Merge the metrics from primer/gene finding
+
     :param str basedir: the basedirectory
     :param str temp_metric_file: the temp metric file with the overall demultiplex stats
     :param str metric_file: the path to the metric file
     :param str metric_file_cell: the path to the metric file with info for each cell
     :param str sample_name: the sample name
+    :param bool: wts: Whether whole transcriptome or not
     :param int ncells: the number of cell indices
     '''
     i = 0
@@ -57,6 +73,8 @@ def merge_metric_files_wts(basedir,temp_metric_file,metric_file,metric_file_cell
             metric,val = line.strip('\n').split(':')
             metric_dict[metric] = float(val)
 
+    choice = 'num_genes_annotated' if wts else 'num_primers_found'
+    
     ## Get Alignment Stats
     files = glob.glob(os.path.join(basedir,"*/read_stats.txt"))
     for f in files:
@@ -64,7 +82,7 @@ def merge_metric_files_wts(basedir,temp_metric_file,metric_file,metric_file_cell
         with open(f,'r') as IN:
             for line in IN:
                 metric,val = line.strip('\n').split(':')
-                if metric != 'num_genes_annotated':
+                if metric != choice:
                     if metric not in metric_dict:
                         metric_dict[metric]=int(val)
                     else:
@@ -85,122 +103,88 @@ def merge_metric_files_wts(basedir,temp_metric_file,metric_file,metric_file_cell
         for metric,val in metric_dict.items():
             out = metric+': {}\n'.format(float_to_string(round(val,2)))
             M.write(out)
-        ## Calc percentage of reads for which genes were annotated
-        temp=(float(metric_dict['num_reads_used'])/metric_dict['num_reads_demultiplexed_for_alignment'])*100
+
+        choice = 'num_reads_used' if wts else 'num_reads_primer_found'
+        ## Calc percentage of reads for which genes/primers were annotated
+        temp=(float(metric_dict[choice])/metric_dict['num_reads_demultiplexed_for_alignment'])*100
         M.write('perc_reads_used: {}\n'.format(float_to_string(round(temp,2))))
 
     ## Write metrics for each cell to file
-    write_metrics_cells_wts(metric_dict_per_cell,ncells,sample_name,metric_file_cell)
+    write_metrics_cells(metric_dict_per_cell,ncells,sample_name,metric_file_cell,wts)
 
-def float_to_string(val):
-    return ('%.2f' % val).rstrip('0').rstrip('.')
-
-def write_metrics_cells_wts(metric_dict_per_cell,ncells,sample_name,outfile):
+def write_metrics_cells(cell_metrics,ncells,sample_name,outfile,wts):
     ''' Write metrics for each cell
 
     :param dict of dict: metric_dict_per_cell: ['cell']['metric'] -> val
     :param int: ncells: the number of cells
     :param str: sample_name: the name of the sample
     :param str: outfile: the output file to write to
+    :param bool: wts: Whether whole transcriptome or not
     :return None
     '''
     cells = range(1,ncells+1)
-    with open(outfile,'w') as OUT:
+    if wts:
         header = (
-            "Cell\tTotal_reads\tTotal_pass_QC_reads\tDetected_genes\tMapped_reads\t"
-            "Uniquely_Mapped_reads\tUsed_reads\tUsed_reads_ERCC\tMapped_ratio\t"
-            "Used_ratio\n"
+            "Cell\tTotal_reads\tTotal_pass_QC_reads\tDetected_genes\t"
+            "Mapped_reads\tUniquely_Mapped_reads\tUsed_reads\tUsed_reads_ERCC"
+            "\tMapped_ratio\tUsed_ratio\n"
         )
+    else:            
+        header = (
+            "Cell\tTotal_reads\tTotal_pass_QC_reads\tDetected_primers\t"
+            "Primers_offtarget\tPrimers_Mismatch\tPrimers_offloci\t"
+            "Primers_unmapped\tUsed_ratio\n"
+        )
+    with open(outfile,'w') as OUT:
         OUT.write(header)
         for cell in cells:
             cell = str(cell)
             key = 'Cell'+cell+'_'+sample_name
             if cell not in metric_dict_per_cell: ## Cell had no reads in demultiplexing
-                out = key+'\t'+'\t'.join(['0']*10)+'\n'
+                out = key+'\t'+'\t'.join(['0']*(len(header)-1))+'\n'
                 OUT.write(out)
             else:
-                num_reads = metric_dict_per_cell[cell]['num_reads']
-                after_qc_reads = metric_dict_per_cell[cell]['after_qc_reads']
-                num_reads_mapped = metric_dict_per_cell[cell]['num_reads_mapped']
-                num_reads_multimapped = metric_dict_per_cell[cell]['num_reads_multimapped']
-                num_reads_unknown_chrom = metric_dict_per_cell[cell]['num_reads_unknown_chrom']
-                num_reads_not_annotated = metric_dict_per_cell[cell]['num_reads_not_annotated']
-                num_reads_used = metric_dict_per_cell[cell]['num_reads_used']
-                num_reads_used_ercc = metric_dict_per_cell[cell]['num_reads_used_ercc']
-                num_genes_annotated = metric_dict_per_cell[cell]['num_genes_annotated']
-                out = [
-                    key,str(num_reads),str(after_qc_reads),
-                    str(num_genes_annotated),str(num_reads_mapped),
-                    str(num_reads_mapped-num_reads_multimapped),
-                    str(num_reads_used),str(num_reads_used_ercc),
-                    float_to_string(round(float(num_reads_mapped)/after_qc_reads,2)),
-                    float_to_string(round(float(num_reads_used)/after_qc_reads,2))
-                ]
+                if wts:
+                    out =
+                    [
+                        key , str(cell_metrics[cell]['num_reads']),
+                        str(cell_metrics[cell]['after_qc_reads']),
+                        str(cell_metrics[cell]['num_genes_annotated']),
+                        str(cell_metrics[cell]['num_reads_mapped']),
+                        str(cell_metrics[cell]['num_reads_mapped'] - \
+                            cell_metrics[cell]['num_reads_multimapped']),
+                        str(cell_metrics[cell]['num_reads_used']),
+                        str(cell_metrics[cell]['num_reads_used_ercc']),
+                        float_to_string(
+                            round(float(
+                                cell_metrics[cell]['num_reads_mapped']) / \
+                                  cell_metrics[cell]['after_qc_reads'],2)),
+                        float_to_string(
+                            round(float(
+                                cell_metrics[cell]['num_reads_used']) / \
+                                  cell_metrics[cell]['after_qc_reads'],2))
+                    ]
+                else:
+                    out =
+                    [
+                        key, str(cell_metrics[cell]['num_reads']),
+                        str(cell_metrics[cell]['after_qc_reads']),
+                        str(cell_metrics['num_primers_found']),
+                        str(cell_metrics['num_reads_primer_offtarget']),
+                        str(cell_metrics['num_reads_primer_mismatch']),
+                        str(cell_metrics['num_reads_primer_off_loci']),
+                        str(cell_metrics['num_reads_unmapped']),
+                        str(cell_metrics['num_reads_endogenous_seq_not_matched']),
+                        str(cell_metrics['num_reads_primer_found'])
+                        float_to_string(
+                            round(float(
+                                cell_metrics[cell]['num_reads_primer_found']) / \
+                                  cell_metrics[cell]['after_qc_reads'],2))                    ]
+
+                    ]
                 OUT.write('\t'.join(out))
                 OUT.write('\n')
-def merge_count_files(basedir,out_file,sample_name,ncells=96):
-    '''
-    :param str basedir: the basedirectory
-    :param str out_file: the path to the output file
-    :param int ncells: the number of cell indices
-    '''
-    i = 0
-    MT = defaultdict(lambda:defaultdict(list))
-    cells = range(1,ncells+1)
-    cell_header = '\t'.join(sample_name+'_Cell'+str(cell) for cell in cells)
-    files = glob.glob(os.path.join(basedir,"*/mt_count.txt"))
-
-    for f in files:
-        cell = os.path.dirname(f).split('/')[-1].split('_')[0].strip('Cell')
-        with open(f,'r') as IN:
-            for line in IN:
-                chrom,start,stop,seq,strand,gene,mt = line.rstrip('\n').split('\t')
-                MT[seq][cell] = [chrom,start,stop,seq,gene,mt]
-
-    last_key = cell ## Store the last cell from the previous loop to get primer info from the dictionary
-    with open(out_file,'w') as OUT:
-        print >> OUT,"chromosome\tstart\tstop\tsequence\tgene\t%s"%cell_header
-        for key in MT:
-            out='\t'.join(MT[key][last_key][0:-1])
-            for cell in cells:
-                if str(cell) not in MT[key]:
-                    out = out + '\t0'
-                else:
-                    #print cell
-                    out=out+'\t'+MT[key][str(cell)][-1]
-            print >> OUT,out
-
-def merge_metric_files(basedir,metric_file,ncells=96):
-    ''' Merge the primer finding metrics
-    :param str basedir: the basedirectory
-    :param str metic_file: the path to the metric file
-    :param int ncells: the number of cell indices
-    '''
-    i = 0
-    cells = range(1,ncells+1)
-    files = glob.glob(os.path.join(basedir,"*/read_stats.txt"))
-    metric_dict = defaultdict(int)
-
-    for f in files:
-        cell = os.path.dirname(f).split('/')[-1].split('_')[0].strip('Cell')
-        with open(f,'r') as IN:
-            for line in IN:
-                metric,val = line.strip('\n').split(':')
-                metric_dict[metric]+=int(val)
-
-    temp_dict = {}
-    with open(metric_file,'r') as M:
-        for line in M:
-            metric,val = line.strip('\n').split(':')
-            temp_dict[metric] = float(val)
-
-    with open(metric_file,'a') as M:
-        for metric,val in metric_dict.items():
-            print >> M,metric+': '+str(val)
-
-        ## Calc percentage of reads for which primers were found
-        temp=(float(metric_dict['num_reads_primer_found'])/temp_dict['num_reads'])*100
-        print >> M,'perc_reads_found_primer: '+str(temp)
-
+                assert len(headers) == len(out), "Error in Column Lengths!!"
+                
 if __name__ == '__main__':
     merge_count_files(sys.argv[1],sys.argv[2])
