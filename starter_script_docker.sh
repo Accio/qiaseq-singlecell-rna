59fef9ec1d713ee121d52d1e16c09aaf6545a295
Raghavendra Padmanabhan <raghus606@gmail.com>
2017-08-29 18:13:29 -0400

    update starter
    Former-commit-id: 9e2e745937bb7b01de4d78a4c80bb2a9d52108a1

--- starter_script_docker.sh
+++ starter_script_docker.sh
@@ -2,6 +2,8 @@ pipeline=$1
 samples=$2
 
 luigid --background --pidfile ~/luigi_scheduler_new/luigi_pid --logdir ~/luigi_scheduler_new/ --state-path ~/luigi_scheduler_new/luigi_state &
+sleep 10
+ps -ef
 cd  /srv/qgen/code/qiaseq-singlecell-rna
 export LUIGI_CONFIG_PATH=${pipeline}
 PYTHONPATH=$PYTHONPATH:"" luigi --module single_cell_rnaseq CombineSamples --samples-cfg ${samples} --worker-wait-interval 20 --workers 16
