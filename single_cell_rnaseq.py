ea2e223a0f3d64e4ebea3f98d3b13596cd13ac86
Raghavendra Padmanabhan <raghus606@gmail.com>
2017-12-15 16:16:06 -0500

    Fixed syntax errors
    Former-commit-id: e99f70268d3ff6f25159a31cd9a8c6386d2e0748

--- single_cell_rnaseq.py
+++ single_cell_rnaseq.py
@@ -484,9 +484,9 @@ class CombineSamples(luigi.Task):
         super(CombineSamples,self).__init__(*args,**kwargs)
 	self.runid = os.path.basename(self.output_dir)
         self.combined_count_file = os.path.join(self.output_dir,'{}.umi_counts.gene.txt'.format(self.runid))
-        self.combined_count_file_primers = os.path.join(self.output_dir.'{runid}.umi_counts.primer.{pcatn}.txt'.format(runid=self.runid,pcatn=config().catalog_number))        
-        self.combined_cell_metrics_file = os.path.join(self.output_dir.'{}.metrics.by_cell_index.txt'.format(self.runid))
-        self.combined_sample_metrics_file = os.path.join(self.output_dir.'{}.metrics.by_sample_index.txt'.format(self.runid))
+        self.combined_count_file_primers = os.path.join(self.output_dir,'{runid}.umi_counts.primer.{pcatn}.txt'.format(runid=self.runid,pcatn=config().catalog_number))        
+        self.combined_cell_metrics_file = os.path.join(self.output_dir,'{}.metrics.by_cell_index.txt'.format(self.runid))
+        self.combined_sample_metrics_file = os.path.join(self.output_dir,'{}.metrics.by_sample_index.txt'.format(self.runid))
         ## The verification file for this task
         self.target_dir = os.path.join(self.output_dir,'targets')
         if not os.path.exists(self.target_dir):
