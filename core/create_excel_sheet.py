fb35f7f43f26aa34c5143a0c850ea86c36f3984b
rpadmanabhan <raghus606@gmail.com>
2017-12-20 19:36:43 +0000

    Updated sheet name for polyA reads
    Former-commit-id: 120324ae55020f31b6b6b279bcecb876a0d643ed

--- core/create_excel_sheet.py
+++ core/create_excel_sheet.py
@@ -23,16 +23,19 @@ def file_reader(infile):
             yield type_casted_row
             #yield [ast.literal_eval(e) for e in line.strip('\n').split('\t')] ## Typecasting for right aligning numbers in excel
 
-def write_excel_workbook(files_to_write,output_excel,catalog_number=None):
+def write_excel_workbook(files_to_write,output_excel,catalog_number=None,species=None):
     ''' Write the give files as an excel workbook with different sheets
     :param list files_to_write: the list of tsv files to write
     :param str output_excel: the output excel file path
     :param str catalog_number: specify a catalog number, if applicable. The gene count excel sheet will be named so
+    :param str catalog_number: specify a species name, if applicable. The gene count excel sheet will be have this
     '''
     workbook = Workbook(output_excel)
     for infile in files_to_write:
         if infile.find("gene") != -1 and catalog_number: ## Gene count file with a catalog number
             sheet_name = "umis.genes."+catalog_number
+        elif infile.find("gene") != -1 and species: ## Gene count file with a species (i.e. polyA)
+            sheet_name = "umis.genes.polyA-"+species.lower()
         elif infile.find("primer") != -1: ## Primer count file
             sheet_name = "umis.primers."+catalog_number
         elif infile.find(".metrics.by_sample_index") != -1: ## Sample Index metrics
