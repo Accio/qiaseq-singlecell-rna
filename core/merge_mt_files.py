7a14bdde88a0eb7e3000a251cf9d92292d336062
rpadmanabhan <raghus606@gmail.com>
2018-11-07 23:29:39 +0000

    Fixed metric calculation

--- core/merge_mt_files.py
+++ core/merge_mt_files.py
@@ -114,8 +114,7 @@ def write_metrics_sample(sample_metrics,outfile,editdistance,wts):
     ## Check to make sure the metrics add up
     reads_total = int(sample_metrics['reads total'])
     ## Updated metrics to account for all N reads
-    dropped_metric = 'reads dropped, cell id not matching a used oligo within edit distance {} bp'.format(editdistance) 
-    sample_metrics['reads dropped, cell id not extracted'] = sample_metrics['reads dropped, cell id not extracted'] - sample_metrics['reads dropped, all NNNNNN sequence']
+    dropped_metric = 'reads dropped, cell id not matching a used oligo within edit distance {} bp'.format(editdistance)
     reads_dropped_demultiplexing = (
         int(sample_metrics['reads dropped, cell id not extracted']) + \
         int(sample_metrics[dropped_metric]) + \
