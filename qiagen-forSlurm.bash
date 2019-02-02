68a98408af16e5de51112da14b90f7ccfed6db05
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-02 09:59:59 +0100

    use two workers

--- qiagen-forSlurm.bash
+++ qiagen-forSlurm.bash
@@ -10,4 +10,4 @@ source activate QiagenSingleRna
 ml load STAR
 export LUIGI_CONFIG_PATH='/pstore/data/biomics/_pre_portfolio/_platform_evaluation/7788_LowInputEvaluation_3UPXQiagen/3UPXQiagenTest_19.12.2018/QiagenSinglecellRna/qiaseq-singlecell-rna/pipeline.cfg'
 luigid &
-PYTHONPATH='.' luigi --module single_cell_rnaseq WriteExcelSheet --samples-cfg /pstore/data/biomics/_pre_portfolio/_platform_evaluation/7788_LowInputEvaluation_3UPXQiagen/3UPXQiagenTest_19.12.2018/QiagenSinglecellRna/qiaseq-singlecell-rna/samples.cfg --workers 12
+PYTHONPATH='.' luigi --module single_cell_rnaseq WriteExcelSheet --samples-cfg /pstore/data/biomics/_pre_portfolio/_platform_evaluation/7788_LowInputEvaluation_3UPXQiagen/3UPXQiagenTest_19.12.2018/QiagenSinglecellRna/qiaseq-singlecell-rna/samples.cfg --workers 2
